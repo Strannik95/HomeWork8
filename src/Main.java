74e00b9cd0eb8d48daf10f6fc7c8178f955c4c68
Manap <Manap1995@mail.ru>
2022-08-26 10:31:27 +0300

    Домашнее Задание 8 с исправлениями 2

--- src/Main.java
+++ src/Main.java
@@ -3,49 +3,49 @@ import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) {
 
-        getLeapYear(2021);
-        getClientOS(1);
-        getDelDis(95);
+        printLeapYear(2019);
+        printClientOS(1, LocalDate.now().getYear());
+        System.out.println("Потребуется дней: - " + printDelDis( 95));
+
     }
     // Задание 1
-    private static int getLeapYear(int year){
+    private static void printLeapYear(int year){
         if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
             System.out.println(year + " год является високосным");
         } else {
             System.out.println(year + " год не является високосным");
         }
-        return year;
     }
 
     // Задание 2
-    private static int getClientOS(int clientOs) {
-
-            int currentYear = LocalDate.now().getYear();
-        if (clientOs == 1 && currentYear > 2015) {
+    private static void printClientOS(int clientOs, int currentYear ) {
+        if (clientOs == 1) {
+            if (currentYear < 2015){
                 System.out.println("«Установите облегченную версию приложения для Android по ссылке».");
             } else {
                 System.out.println("«Установите версию приложения для Android по ссылке».");
             }
-
-            if (clientOs == 0 && currentYear > 2015) {
-                System.out.println("«Установите облегченную версию приложения для iOS по ссылке».");
+        } else {
+            if (clientOs == 0) {
+                if (currentYear < 2015) {
+                    System.out.println("«Установите облегченную версию приложения для iOS по ссылке».");
                 } else {
                     System.out.println("«Установите версию приложения для iOS по ссылке».");
                 }
-            return clientOs;
+            }
+        }
         }
          // Задание 3
-     private static int getDelDis(int dayDel){
+     private static int printDelDis(int deliveryDistance){
           int deliveryDays = 1;
 
-          if (dayDel > 20){
+          if (deliveryDistance > 20){
               deliveryDays++;
           }
-          if (dayDel > 60){
+          if (deliveryDistance > 60){
               deliveryDays++;
           }
-          System.out.println("потребуется дней  " + deliveryDays);
-          return dayDel;
+          return deliveryDays;
       }
 
     }
